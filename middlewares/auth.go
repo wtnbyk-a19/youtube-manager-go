b25694bd086033d88e276202a3014ec14639e8c9
Yuki Watanabe <>
2021-01-23 22:42:43 +0900

    Update auth.go

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -6,6 +6,7 @@ import (
 
 	"firebase.google.com/go/auth"
 	"github.com/labstack/echo"
+	"github.com/valyala/fasthttp"
 )
 
 func verifyFirebaseIDToken(ctx echo.Context, auth *auth.Client) (*auth.Token, error) {
@@ -14,4 +15,42 @@ func verifyFirebaseIDToken(ctx echo.Context, auth *auth.Client) (*auth.Token, er
 	jwtToken, err := auth.VerifyIDToken(context.Background(), token)
 
 	return jwtToken, err
+}
+
+func FirebaseGuard() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			authClient := c.Get("firebase").(*auth.Client)
+			jwtToken, err := verifyFirebaseIDToken(c, authClient)
+
+			if err != nil {
+				return c.JSON(fasthttp.StatusUnauthorized, "Not Authenticated")
+			}
+
+			c.Set("auth", jwtToken)
+
+			if err := next(c); err != nil {
+				return err
+			}
+
+			return nil
+		}
+	}
+}
+
+func FirebaseAuth() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			authClient := c.Get("firebase").(*auth.Client)
+			jwtToken, _ := verifyFirebaseIDToken(c, authClient)
+
+			c.Set("auth", jwtToken)
+
+			if err := next(c); err != nil {
+				return err
+			}
+
+			return nil
+		}
+	}
 }
