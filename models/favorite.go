7f80b3d2ece568291d50c21b44ce87390590f62d
Yuki Watanabe <>
2021-01-25 21:19:48 +0900

    Fix item name

--- models/favorite.go
+++ models/favorite.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Favorite struct {
-	ID         uint      `gorm:"primary_key"`
-	UserId     uint      `json:"user_id"`
-	VideoId    string    `json:"video_id"`
-	CreatedAt  time.Time `json:"-"`
-	UpadatedAt time.Time `json:"-"`
+	ID        uint      `gorm:"primary_key"`
+	UserId    uint      `json:"user_id"`
+	VideoId   string    `json:"video_id"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 
 	User User
 }
--- models/user.go
+++ models/user.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type User struct {
-	ID         uint       `gorm:"primary_key"`
-	UID        string     `json:"-"`
-	CreatedAt  time.Time  `json:"-"`
-	UpadatedAt time.Time  `json:"-"`
-	DeletedAt  *time.Time `sql:"index"json:"-"`
+	ID        uint       `gorm:"primary_key"`
+	UID       string     `json:"-"`
+	CreatedAt time.Time  `json:"-"`
+	UpdatedAt time.Time  `json:"-"`
+	DeletedAt *time.Time `sql:"index"json:"-"`
 
 	Favorites []Favorite
 }
